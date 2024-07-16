43c93b963e3659eb6efc1dec14af21aa9a9c91ec
Optics-Boss <->
2024-07-16 20:23:16 +0200

    Added harpoon

--- init.lua
+++ init.lua
@@ -107,17 +107,22 @@ require("lazy").setup({
   {
     'alvarosevilla95/luatab.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+  {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" }
   }
 })
 
--- Theme configuration for catppuccin
+-- Theme config
 require("catppuccin").setup({
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+    flavour = "macchiato",
+    background = {
         light = "frappe",
         dark = "macchiato",
     },
-    transparent_background = false, -- disables setting the background color.
+    transparent_background = false,
     integrations = {
         cmp = true,
         gitsigns = true,
@@ -131,10 +136,9 @@ require("catppuccin").setup({
     },
 })
 
--- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
 
--- LSP configs
+-- LSP config
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
     if desc then
@@ -192,7 +196,7 @@ require("lspconfig").tsserver.setup ({
     on_attach = on_attach,
 })
 
--- Cmp configs
+-- Cmp config
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
@@ -243,7 +247,7 @@ cmp.setup {
   },
 }
 
--- Telescope configs
+-- Telescope config
 require('telescope').setup{
   defaults = {
     file_ignore_patterns = {
@@ -268,7 +272,24 @@ require('telescope.builtin').keymaps, { desc = 'Search Keymaps' })
 vim.keymap.set('n', '<leader>sd',
 require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' })
 
--- Luatab
+-- Luatab config
 require('luatab').setup{}
 
+-- Comment config
 require('Comment').setup()
+
+-- Harpoon config
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
